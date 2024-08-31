7e1e29e8007ec9aa721c012eb47f744cb4407f13
pradip <pradip.kharal777@gmail.com>
2024-08-31 23:41:28 +0545

    const data

---
+++ data/index.js
@@ -0,0 +1,23 @@
+import foodSetOne from "./SetOne/foodSetOne";
+import geographySetOne from "./SetOne/geographySetOne";
+import hollywoodSetOne from "./SetOne/hollywoodSetOne";
+import bollywoodSetOne from "./SetOne/bollywoodSetOne";
+import foodSetTwo from "./SetTwo/foodSetTwo";
+import geographySetTwo from "./SetTwo/geographySetTwo";
+import hollywoodSetTwo from "./SetTwo/hollywoodSetTwo";
+import bollywoodSetTwo from "./SetTwo/bollywoodSetTwo";
+
+export const allSets = {
+  setOne: {
+    Food: foodSetOne,
+    Geography: geographySetOne,
+    Hollywood: hollywoodSetOne,
+    Bollywood: bollywoodSetOne,
+  },
+  setTwo: {
+    Food: foodSetTwo,
+    Geography: geographySetTwo,
+    Hollywood: hollywoodSetTwo,
+    Bollywood: bollywoodSetTwo,
+  },
+};
