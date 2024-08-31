7bcbab01ab4c3c9586fb63649d3dddc0cec41088
pradip <pradip.kharal777@gmail.com>
2024-08-31 23:40:32 +0545

    constsnts

---
+++ app/constants/Categories.js
@@ -0,0 +1,16 @@
+import foodSetOne from "../../data/SetOne/foodSetOne";
+import geographySetOne from "../../data/SetOne/geographySetOne";
+import hollywoodSetOne from "../../data/SetOne/hollywoodSetOne";
+import bollywoodSetOne from "../../data/SetOne/bollywoodSetOne";
+
+export const Food = "Food";
+export const Geography = "Geography";
+export const Hollywood = "Hollywood";
+export const Bollywood = "Bollywood";
+
+export const categories = [
+  { name: Food, data: foodSetOne },
+  { name: Geography, data: geographySetOne },
+  { name: Hollywood, data: hollywoodSetOne },
+  { name: Bollywood, data: bollywoodSetOne },
+];
